30b673d091dcabe8b3290d1f4a87ebd9f081ab98
ChesterCraig <chestercraig@Chesters-MacBook-Pro.local>
2017-02-05 14:01:49 +1300

    fixed env.port to env.PORT

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 var app = express();
 var mtncMode = false;
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 console.log("starting app on port " + port);
 
